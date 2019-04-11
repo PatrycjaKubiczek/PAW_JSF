4a5a9c7f59c182797e26a7560a02bae2d3fde7b0
patrycjakubiczek <p.kubiczek@gmail.com>
2019-04-11 22:10:40 +0200

    first commit

---
+++ src/main/java/pl/edu/agh/paw/SimpleForm.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.paw;
+
+/**
+ * Created by student on 2019-04-07.
+ */
+import javax.faces.bean.ManagedBean;
+//import java.util.logging.Logger;
+
+
+@ManagedBean
+public class SimpleForm {
+    private String firstName = "Anna";
+
+
+
+}
